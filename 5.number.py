e32de588334720444499f224075037c8188a5999
Hanieh.ZiaFarshchi <111128760+HaniehZiaFarshchi98@users.noreply.github.com>
2022-08-31 23:48:02 +0430

    Add files via upload

---
+++ 5.number.py
@@ -0,0 +1,45 @@
+import random
+
+min = 1
+max = 100
+guess = random.randint(1 , 100)
+print("my guess is:" , guess)
+print("did number correct? type yes or kuchektar or bozorgtar ")
+answer = input()
+
+while answer != 'yes':
+        
+        if answer == "kuchektar":
+                new_guess = random.randint(guess , max)
+                print("my new guess is:" , new_guess)
+                break
+        elif answer == 'bozorgtar':
+                new_guess = random.randint( min , guess)
+                print("my new guess is:" , new_guess)
+                break
+while answer == 'yes':
+        print('i win')
+        break
+
+
+
+
+
+
+
+
+        
+        
+
+
+    
+    
+
+
+
+
+
+
+
+        
+
